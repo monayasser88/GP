18155998e3a19457a965bbfa084e9f61a685df3c
monayasser88 <monnayaso@gmail.com>
2023-10-23 04:14:45 +0300

    remove unused imports

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,15 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:gp_project/components/custom_appbar.dart';
-import 'package:gp_project/components/custom_container_in_profile.dart';
-import 'package:gp_project/components/custom_edit_photo.dart';
-import 'package:gp_project/components/custom_pop_up.dart';
-import 'package:gp_project/components/custom_title_in_profile.dart';
 import 'package:gp_project/components/profile_view.dart';
-import 'package:gp_project/constraints.dart';
 import 'package:gp_project/cubit/profile_cubit.dart';
-import 'package:gp_project/pages/change_password.dart';
-import 'package:gp_project/pages/update_profile.dart';
+
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
